adbb8fe36204ae20c59351a53d775d494fad9d48
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-02 13:24:33 +0200

    Add configure service for API, Application and Infrastructure layer

---
+++ src/API/ConfigureServices.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddAPIServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
