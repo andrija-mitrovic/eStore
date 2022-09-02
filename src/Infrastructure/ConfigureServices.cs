6bbfdf888d76cd7cee5c12375b96fbd2e0fe5ef8
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-02 14:21:15 +0200

    Add database

--- src/Infrastructure/ConfigureServices.cs
+++ src/Infrastructure/ConfigureServices.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ConfigureServices
     {
+        private const string CONNECTION_STRING_NAME = "DefaultConnection";
+
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
+            AddDatabase(services, configuration);
+
             return services;
+        }
+
+        private static void AddDatabase(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(configuration.GetConnectionString(CONNECTION_STRING_NAME),
+                    builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
         }
     }
 }
