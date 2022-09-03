3353ee32d41acc5c7423fbf06bab97078a2a0a23
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-03 08:51:10 +0200

    Add code to initialise database in program.cs

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,3 +1,5 @@
+using Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,12 +14,18 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    // Initialise and seed database
+    using (var scope = app.Services.CreateScope())
+    {
+        var initialiser = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitialiser>();
+        await initialiser.InitialiseAsync();
+        await initialiser.SeedAsync();
+    }
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
