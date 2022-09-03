e22c70eedd0d26fced56f728e87f2ec06d85f618
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-03 08:49:39 +0200

    Add product repository

---
+++ src/Infrastructure/Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(ApplicationDbContext dbContext) : base(dbContext) { }
+    }
+}
