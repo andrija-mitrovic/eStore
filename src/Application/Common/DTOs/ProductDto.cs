8f0ebb4d2c3aff0e47579c096558af9a111bd0c9
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-04 07:50:23 +0200

    Add product dto

---
+++ src/Application/Common/DTOs/ProductDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Common.DTOs
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public long Price { get; set; }
+        public string? PictureUrl { get; set; }
+        public string? Type { get; set; }
+        public string? Brand { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+}
