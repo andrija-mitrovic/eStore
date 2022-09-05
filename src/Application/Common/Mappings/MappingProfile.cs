56c5e6c5f21fe9766ba0a8a39db015aa4b9ccf82
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-05 08:46:24 +0200

    Add mapping

---
+++ src/Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common.DTOs;
+using Application.Products.Commands.CreateProduct;
+using Application.Products.Commands.UpdateProduct;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Common.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Product, CreateProductCommand>().ReverseMap();
+            CreateMap<Product, UpdateProductCommand>().ReverseMap();
+        }
+    }
+}
