2d7b8b21344765e168ea23993468118f1a4fe040
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-05 14:20:38 +0200

    Get product by id functionality

---
+++ src/Application/Products/Queries/GetProductById/GetProductByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using Application.Common.DTOs;
+using MediatR;
+
+namespace Application.Products.Queries.GetProductById
+{
+    public sealed class GetProductByIdQuery : IRequest<ProductDto>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/Application/Products/Queries/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using Application.Common.DTOs;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Products.Queries.GetProductById
+{
+    internal sealed class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, ProductDto>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<GetProductByIdQueryHandler> _logger;
+
+        public GetProductByIdQueryHandler(
+            IProductRepository productRepository, 
+            IMapper mapper, 
+            ILogger<GetProductByIdQueryHandler> logger)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<ProductDto> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(request.Id);
+
+            if (product == null)
+            {
+                _logger.LogError(nameof(Product) + " with Id: {ProductId} was not found.", request.Id);
+                throw new NotFoundException(nameof(Product), request.Id);
+            }
+
+            return _mapper.Map<ProductDto>(product);
+        }
+    }
+}
