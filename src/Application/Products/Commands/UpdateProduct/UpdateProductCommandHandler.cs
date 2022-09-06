b1857fb0db70549515075346b903fa5141394bdf
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-06 08:16:04 +0200

    Add update product feature

---
+++ src/Application/Products/Commands/UpdateProduct/UpdateProductCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Products.Commands.UpdateProduct
+{
+    internal sealed class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UpdateProductCommandHandler> _logger;
+
+        public UpdateProductCommandHandler(
+            IProductRepository productRepository, 
+            IUnitOfWork unitOfWork, 
+            IMapper mapper, 
+            ILogger<UpdateProductCommandHandler> logger)
+        {
+            _productRepository = productRepository;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(request.Id);
+
+            if (product == null)
+            {
+                _logger.LogError(nameof(Product) + " with Id: {ProductId} was not found.", request.Id);
+                throw new NotFoundException(nameof(Product), request.Id);
+            }
+
+            _mapper.Map(request, product);
+
+            _productRepository.Update(product);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Application/Products/Commands/UpdateProduct/UpdateProductCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Application.Products.Commands.UpdateProduct
+{
+    internal sealed class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Description).NotEmpty();
+            RuleFor(x => x.PictureUrl).NotEmpty();
+            RuleFor(x => x.Brand).NotEmpty();
+            RuleFor(x => x.Type).NotEmpty();
+            RuleFor(x => x.Price).GreaterThan(0);
+            RuleFor(x => x.QuantityInStock).GreaterThan(0).When(x => x != null);
+        }
+    }
+}
