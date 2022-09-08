97416d006691e7eccfc0a7fe120e7e90454970ee
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-08 09:41:11 +0200

    Update header

--- src/Web/client/src/app/layout/Header.tsx
+++ src/Web/client/src/app/layout/Header.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function Header({ darkMode, handleThemeChange }: Props) {
   return (
-    <AppBar>
+    <AppBar position="static"  sx={{ mb: 4 }}>
       <Toolbar>
         <Typography variant="h6">E-STORE</Typography>
         <Switch checked={darkMode} onChange={handleThemeChange} />
