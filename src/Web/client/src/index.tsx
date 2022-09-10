713e9e0e6fc56dff0173b8773c1f6613ae07dbcf
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-10 09:15:47 +0200

    Add notification

--- src/Web/client/src/index.tsx
+++ src/Web/client/src/index.tsx
@@ -3,15 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./app/layout/styles.css";
 import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Router } from "react-router-dom";
+import { createBrowserHistory } from 'history';
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+export const history = createBrowserHistory();
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Router history={history}>
+        <App />
+      </Router>
     </BrowserRouter>
   </React.StrictMode>
 );
