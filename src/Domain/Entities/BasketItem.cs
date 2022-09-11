93fa65f1d725df8b7fba5f81c4660c5c494a9323
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-11 09:42:39 +0200

    Add BasketItem entity

---
+++ src/Domain/Entities/BasketItem.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class BasketItem : BaseEntity
+    {
+        public int Quantity { get; set; }
+        public Basket? Basket { get; set; }
+        public int BasketId { get; set; }
+        public Product? Product { get; set; }
+        public int ProductId { get; set; }
+    }
+}
