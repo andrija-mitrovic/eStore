289d5611e5bb3624fc5c0782a3f10c748fdeddd2
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-11 09:55:42 +0200

    Add two methods AddItem and RemoveItem in Basket entity

--- src/Domain/Entities/Basket.cs
+++ src/Domain/Entities/Basket.cs
@@ -6,5 +6,34 @@ namespace Domain.Entities
     {
         public string? BuyerId { get; set; }
         public List<BasketItem> Items { get; set; } = new();
+
+        public void AddItem(Product product, int quantity)
+        {
+            if (Items.All(x => x.ProductId != product.Id))
+            {
+                Items.Add(new BasketItem() { Product = product, Quantity = quantity });
+            }
+
+            var existingItem = Items.FirstOrDefault(x => x.ProductId == product.Id);
+
+            if (existingItem != null)
+            {
+                existingItem.Quantity += quantity;
+            }
+        }
+
+        public void RemoveItem(int productId, int quantity)
+        {
+            var item = Items.FirstOrDefault(x => x.ProductId == productId);
+
+            if (item == null) return;
+
+            item.Quantity -= quantity;
+
+            if (item.Quantity == 0)
+            {
+                Items.Remove(item);
+            }
+        }
     }
 }
