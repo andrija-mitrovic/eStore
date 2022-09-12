e2b9b946922546190244bb2bfd4c29e4b0fa85cd
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-12 08:25:11 +0200

    Add class for storing authorization constants

---
+++ src/Application/Common/Constants/AuthorizationConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Common.Constants
+{
+    public class AuthorizationConstants
+    {
+        public const string AUTH_KEY = "AuthKeyOfDoomThatMustBeAMinimumNumberOfBytes";
+        public const string DEFAULT_PASSWORD = "Pa$$w0rd";
+        public const string JWT_SECRET_KEY = "SecretKeyOfDoomThatMustBeAMinimumNumberOfBytes";
+    }
+}
