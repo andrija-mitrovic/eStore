9148ff7e7a2888f33eedd056315e5a0fd132c05e
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-13 08:15:56 +0200

    Add cookie service

---
+++ src/Application/Common/Interfaces/ICookieService.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Common.Interfaces
+{
+    public interface ICookieService
+    {
+        public void AddCookie(string key, string value);
+    }
+}
---
+++ src/Application/Common/Services/CookieService.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Common.Services
+{
+    public class CookieService : ICookieService
+    {
+        private const int EXPIRY_DAYS = 30;
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CookieService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public void AddCookie(string key, string value)
+        {
+            var cookieOptions = new CookieOptions
+            {
+                IsEssential = true,
+                Expires = DateTime.Now.AddDays(EXPIRY_DAYS)
+            };
+
+            _httpContextAccessor.HttpContext.Response.Cookies.Append(key, value, cookieOptions);
+        }
+    }
+}
