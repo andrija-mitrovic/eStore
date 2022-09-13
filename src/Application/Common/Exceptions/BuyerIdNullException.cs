ca7f818f3805686528a271da02a0baa58c46521b
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-13 08:16:20 +0200

    Add buyerId null exception

---
+++ src/Application/Common/Exceptions/BuyerIdNullException.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Common.Exceptions
+{
+    public class BuyerIdNullException : Exception
+    {
+        public BuyerIdNullException() : base()
+        {
+        }
+
+        public BuyerIdNullException(string message)
+            : base(message)
+        {
+        }
+    }
+}
