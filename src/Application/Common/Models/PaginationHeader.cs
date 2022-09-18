cd647ff07630d933693ae49e54871f9b4060a1f3
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-18 21:11:24 +0200

    Add pagination header

---
+++ src/Application/Common/Models/PaginationHeader.cs
@@ -0,0 +1,22 @@
+﻿namespace Application.Common.Models
+{
+    public class PaginationHeader
+    {
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public int TotalPages { get; set; }
+
+        public PaginationHeader(
+            int currentPage, 
+            int itemsPerPage, 
+            int totalPages, 
+            int totalItems)
+        {
+            this.CurrentPage = currentPage;
+            this.PageSize = itemsPerPage;
+            this.TotalPages = totalPages;
+            this.TotalCount = totalItems;
+        }
+    }
+}
