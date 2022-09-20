dbc77000e56f67999f5f559a57e8300eae9fec55
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-20 09:16:43 +0200

    Add http extensions

---
+++ src/Application/Common/Extensions/HttpExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Application.Common.Models;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Application.Common.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader(this HttpResponse response, int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalPages, totalItems);
+            var camelCaseFormatter = new JsonSerializerSettings();
+            camelCaseFormatter.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader, camelCaseFormatter));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
+    }
+}
