872e026776afb9a958f5708dad1d25f8e6e16d2e
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-09-26 09:15:44 +0200

    Add account controller

---
+++ src/API/Controllers/AccountsController.cs
@@ -0,0 +1,80 @@
+﻿using Application.Common.Constants;
+using Application.Common.DTOs;
+using Application.Common.Interfaces;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountsController : ApiControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ITokenService _tokenService;
+
+        public AccountsController(
+            UserManager<ApplicationUser> userManager,
+            ITokenService tokenService)
+        {
+            _userManager = userManager;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByNameAsync(loginDto.Username);
+
+            if (user == null || !await _userManager.CheckPasswordAsync(user, loginDto.Password))
+            {
+                return Unauthorized();
+            }
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = await _tokenService.GenerateTokenAsync(user.UserName, user.Email)
+            };
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterDto registerDto)
+        {
+            var user = new ApplicationUser 
+            { 
+                UserName = registerDto.Username, 
+                Email = registerDto.Email 
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+
+                return ValidationProblem();
+            }
+
+            await _userManager.AddToRoleAsync(user, RolesConstants.MEMBER);
+
+            return StatusCode(201);
+        }
+
+        [Authorize]
+        [HttpGet("currentUser")]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity?.Name);
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = await _tokenService.GenerateTokenAsync(user.UserName, user.Email)
+            };
+        }
+    }
+}
--- src/API/Program.cs
+++ src/API/Program.cs
@@ -27,6 +27,7 @@ if (app.Environment.IsDevelopment())
 app.UseCors(opt => { opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000"); });
 app.UseHealthChecks("/health");
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
