0b6670c45933f41ff3d6dc7f575ea5f45037af1f
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-10-12 08:43:24 +0200

    Add orders controller

---
+++ src/API/Controllers/OrdersController.cs
@@ -0,0 +1,37 @@
+﻿using Application.Orders.Commands.CreateOrder;
+using Application.Orders.Queries.GetOrderById;
+using Application.Orders.Queries.GetOrders;
+using Domain.Entities.OrderAggregate;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class OrdersController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Order>>> GetOrders()
+        {
+            var query = new GetOrdersQuery() 
+            { 
+                BuyerId = User?.Identity?.Name 
+            };
+
+            return await Mediator.Send(query);
+        }
+        
+        [HttpGet("{id}", Name = "GetOrder")]
+        public async Task<ActionResult<Order?>> GetOrder(int id)
+        {
+            var query = new GetOrderByIdQuery() 
+            { 
+                Id = id, 
+                BuyerId = User?.Identity?.Name 
+            };
+
+            return await Mediator.Send(query);
+        }
+
+    }
+}
