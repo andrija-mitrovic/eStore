2f37786828a6d18dcde504957c0e4627602c3c91
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-10-12 08:44:10 +0200

    Add user repository

---
+++ src/Application/Common/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities.OrderAggregate;
+
+namespace Application.Common.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task UpdateUserAddress(string username, ShippingAddress address);
+    }
+}
---
+++ src/Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using Domain.Entities.OrderAggregate;
+using Infrastructure.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class UserRepository : GenericRepository<ApplicationUser>, IUserRepository
+    {
+        public UserRepository(ApplicationDbContext dbContext) : base(dbContext) { }
+
+        public async Task UpdateUserAddress(string username, ShippingAddress address)
+        {
+            var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.UserName == username);
+
+            if (user == null) throw new NotFoundException($"User with username {username} was not found.");
+
+            user.Address = new UserAddress
+            {
+                FullName = address.FullName,
+                Address1 = address.Address1,
+                Address2 = address.Address2,
+                City = address.City,
+                State = address.State,
+                Zip = address.Zip,
+                Country = address.Country
+            };
+
+            await _dbContext.Users.AddAsync(user);
+        }
+    }
+}
