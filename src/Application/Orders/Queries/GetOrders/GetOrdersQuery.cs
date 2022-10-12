530f8cc939ebae1ced5a43fd1beb4e40d8fc63a5
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-10-12 08:45:15 +0200

    Add get orders feature

---
+++ src/Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities.OrderAggregate;
+using MediatR;
+
+namespace Application.Orders.Queries.GetOrders
+{
+    public sealed class GetOrdersQuery : IRequest<List<Order>>
+    {
+        public string? BuyerId { get; set; }
+    }
+}
---
+++ src/Application/Orders/Queries/GetOrders/GetOrdersQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities.OrderAggregate;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Orders.Queries.GetOrders
+{
+    internal sealed class GetOrdersQueryHandler : IRequestHandler<GetOrdersQuery, List<Order>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetOrdersQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Order>> Handle(GetOrdersQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Orders.Include(x=>x.OrderItems)
+                                        .Where(x=>x.BuyerId == request.BuyerId)
+                                        .ToListAsync();
+        }
+    }
+}
