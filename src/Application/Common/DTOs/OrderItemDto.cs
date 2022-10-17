f5d10ada4adc25256f60ce54cb7319c82f5fa6d7
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-10-17 11:09:24 +0200

    Add order item dto

---
+++ src/Application/Common/DTOs/OrderItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Common.DTOs
+{
+    public sealed class OrderItemDto
+    {
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public string? PictureUrl { get; set; }
+        public long Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
