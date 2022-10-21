4acc7b06b408ad42e7e9561f1ebdd5cee6e4251f
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-10-21 08:42:14 +0200

    Add validation exception test

---
+++ tests/Application.UnitTests/Common/Exceptions/ValidationExceptionTest.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+using ValidationException = Application.Common.Exceptions.ValidationException;
+
+namespace Application.UnitTests.Common.Exceptions
+{
+    public sealed class ValidationExceptionTest
+    {
+        [Fact]
+        public void DefaultConstructorCreatesAnEmptyErrorDictionary()
+        {
+            var actual = new ValidationException().Errors;
+
+            actual.Keys.Should().BeEquivalentTo(Array.Empty<string>());
+        }
+
+
+    }
+}
