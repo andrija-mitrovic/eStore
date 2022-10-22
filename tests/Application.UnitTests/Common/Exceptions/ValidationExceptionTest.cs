b56fa6d0dad675b9d9fb93543e6e7151020b203d
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-10-22 09:15:35 +0200

    Update validation exception test

--- tests/Application.UnitTests/Common/Exceptions/ValidationExceptionTest.cs
+++ tests/Application.UnitTests/Common/Exceptions/ValidationExceptionTest.cs
@@ -1,5 +1,7 @@
 ﻿using FluentAssertions;
+using FluentValidation.Results;
 using System;
+using System.Collections.Generic;
 using Xunit;
 using ValidationException = Application.Common.Exceptions.ValidationException;
 
@@ -15,6 +17,39 @@ namespace Application.UnitTests.Common.Exceptions
             actual.Keys.Should().BeEquivalentTo(Array.Empty<string>());
         }
 
+        [Fact]
+        public void SingleValidationFailureCreatesASingleElementErrorDictionary()
+        {
+            var failures = new List<ValidationFailure>
+            {
+                new ValidationFailure("QuantityInStock", "Quantity in stock cannot be less than zero")
+            };
 
+            var actual = new ValidationException(failures).Errors;
+
+            actual.Keys.Should().BeEquivalentTo(new string[] { "QuantityInStock" });
+            actual["QuantityInStock"].Should().BeEquivalentTo(new string[] { "Quantity in stock cannot be less than zero" });
+        }
+
+        [Fact]
+        public void MulitpleValidationFailureForMultiplePropertiesCreatesAMultipleElementErrorDictionaryEachWithMultipleValues()
+        {
+            var failures = new List<ValidationFailure>
+            {
+                new ValidationFailure("QuantityInStock", "Quantity in stock cannot be less than zero"),
+                new ValidationFailure("QuantityInStock", "Quantity in stock must be greater than zero"),
+                new ValidationFailure("Name", "Name cannot be empty")
+            };
+
+            var actual = new ValidationException(failures).Errors;
+
+            actual.Keys.Should().BeEquivalentTo(new string[] { "QuantityInStock", "Name" });
+            actual["QuantityInStock"].Should().BeEquivalentTo(new string[]
+            {
+                "Quantity in stock cannot be less than zero",
+                "Quantity in stock must be greater than zero"
+            });
+            actual["Name"].Should().BeEquivalentTo(new string[] { "Name cannot be empty" });
+        }
     }
 }
